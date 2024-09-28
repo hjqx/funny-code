25cac99f3c5d38516461f2a00de8a684bd987ce8
hjqx <180844972+hjqx@users.noreply.github.com>
2024-09-29 00:11:05 +0200

    Add files via upload

---
+++ o.py
@@ -0,0 +1,6 @@
+v=(r:=lambda a,b,c,: "".join(list(map(chr,[c
+for c in [].__class__(a (lambda x :sum( [int
+(c)for c in str(x)]).__eq__(b+1) and int(c)>
+72,list(range(c,62,-1))))]))))and(x:=(lambda
+:(n:=r))())(filter,list(map(len,['hjqx']))[0
+             ],201);print(v)
