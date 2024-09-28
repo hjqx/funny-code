0722ad2c22e68adb4deb8c300acace96fdc50acd
hjqx <180844972+hjqx@users.noreply.github.com>
2024-09-29 00:11:52 +0200

    Add files via upload

---
+++ z.py
@@ -0,0 +1,6 @@
+# custom infinty loop
+(lambda _, __, ___: [(getattr([].
+       __class__,'append')(_,__)) 
+        for [__, ___][[] > []] in ((lambda _: _)(_))])(
+    [lambda: [*...,] > [lambda e : e]], (lambda x : x) , 0x3
+)
