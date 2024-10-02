e6f9ac6a48ca58790da76379305162543b2dd02e
zqн <180844972+hjqx@users.noreply.github.com>
2024-10-02 17:23:06 +0200

    Create s.py

---
+++ s.py
@@ -0,0 +1,10 @@
+(lambda c, d : ( lambda __, é, à: (
+        ... if c(__, "__setitem__")(
+            "\x0d%s" % "", ((lambda c: (
+        *map(lambda *v: à(v[0] ^ c), (*_,)),)))( int("\x33%s"%2)),
+        ) or ( (lambda 
+             _: c(_("built%s"%'ins'), "print"))
+                         (d))(c('','join')(__['\r']))
+        else globals().clear())
+)(e := {}, _ := (_ for _ in [90, 81, 72]),chr)) (
+getattr,__import__)
